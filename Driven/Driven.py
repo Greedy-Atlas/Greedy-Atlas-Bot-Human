346ea07cf32395d20695d66d084ca6fec6b3663d
diadntjr <dev.dntjr0501@gmail.com>
2023-09-06 20:24:47 +0900

    Update Driven
    Module test part

--- Driven/Driven.py
+++ Driven/Driven.py
@@ -1,6 +1,7 @@
-from .color_seperate import ColorSeperate
+from color_seperate import ColorSeperate
 from pop.Pilot import Object_Follow, Camera, SerBot
 from lidar import Lidar
+from threading import Thread
 import time
 
 class Driven(object):
@@ -25,18 +26,28 @@ class Driven(object):
             self.lidar_detect = 3
 
             self.loaded = False
+            self.run_pause = False
+
+            self.run_t = None
+
             cls.__init = True
 
     def load(self):
         if not self.loaded:
             self.bot = SerBot()
-            self.lidar = Lidar()
+            self.lidar = Lidar(self.length)
+            print("[SerBot] : === Bot, Lidar Setting Complete ===") 
+
             self.cam = Camera(320,320)
             self.of = Object_Follow(self.cam)
             self.of.load_model()
-            print("[SerBot] : === Loaded Complete ===")
+            print("[SerBot] : === Object Follow Module Loaded Complete ===")
             self.colorS = ColorSeperate(self.cam)
             
+            self.run_t = Thread(self.run())
+            self.run_t.start()
+            print("[SerBot] : === Running. ===")
+
             self.loaded = True
         else :
             print("[SerBot] : Already Loaded")
@@ -48,11 +59,22 @@ class Driven(object):
 
             speed = 90 if self.lidar_detect == 0 else 60 if self.lidar_detect == 1 else 30
             self.bot.forward(speed)
+        else: 
+            self.bot.stop()
 
     def run(self):
         self.lidar_detect = self.lidar.check_distance(self.bot.steering)
 
-        if self.lidar_detect != 3 :
+        if self.lidar_detect != 3 and not self.run_pause :
             self.human_detect()
-            self.colorS.x_cor
+            if self.colorS.x_cor == None:
+                self.bot.steering = 1.0 if self.human_x > 1.0 else -1.0 if self.human_x < -1.0 else self.human_x
+            else:
+                self.bot.steering = 1.0 if self.colorS.x_cor> 1.0 else -1.0 if self.colorS.x_cor < -1.0 else self.colorS.x_cor
 
+        else:
+            self.bot.stop()
+
+if __name__ == '__main__' :
+    drv = Driven()
+    drv.load()
