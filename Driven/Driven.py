0b9e59c8bbb02363f4c72d60eddba97255ee80fc
diadntjr <dev.dntjr0501@gmail.com>
2023-09-07 20:26:46 +0900

    Update Driven Class file
    - Change Thread target
    - Move run_pause self variable
    - Add hum_x, colS variable

--- Driven/Driven.py
+++ Driven/Driven.py
@@ -3,7 +3,7 @@ from pop.Pilot import Object_Follow, Camera, SerBot
 from lidar import Lidar
 from threading import Thread
 import time
-import os
+import sys
 
 class Driven(object):
     def __new__(cls, *args, **kwargs):
@@ -54,7 +54,7 @@ class Driven(object):
             print("[SerBot] : === Object Follow Module Loaded Complete ===")
             self.colorS = ColorSeperate(self.cam)
             
-            self.run_t = Thread(target=self.run(), daemon=True)
+            self.run_t = Thread(target=self.run, daemon=True)
             self.run_t.start()
             print("[SerBot] : === Running. ===")
 
@@ -74,28 +74,28 @@ class Driven(object):
 
     def run(self):
         while True:
-            print("[SerBot] : Lidar Scanning")
-            self.lidar_detect = self.lidar.check_distance(self.bot.steering)
+            if not self.run_pause: 
+                self.lidar_detect = self.lidar.check_distance(self.bot.steering)
 
-            if self.lidar_detect != 3 and not self.run_pause :
-                print("[SerBot] : Human Detecting")
-                self.human_detect()
-                if self.colorS.x_cor and self.human_x :
-                    print("[SerBot] : Human Detected")
-                    if self.human_x >= self.colorS.x_cor - 0.05 and self.human_x <= self.colorS.x_cor + 0.05 :
-                        x = self.human_x
-                    else :
-                        x = round(self.colorS.x_cor*4, 1)
-                    self.bot.steering = 1.0 if x > 1.0 else -1.0 if x < -1.0 else x
-                    self.bot.forward()
+                if self.lidar_detect != 3 :
+                    hum_x = self.human_x
+                    colS = self.colorS.x_cor
+                    self.human_detect()
+                    if colS and hum_x :
+                        if hum_x >= colS - 0.05 and hum_x <= colS + 0.05 :
+                            x = hum_x
+                        else :
+                            x = round(colS*4, 1)
+                        self.bot.steering = 1.0 if x > 1.0 else -1.0 if x < -1.0 else x
+                        self.bot.forward()
+                    else:
+                        self.bot.stop()
                 else:
                     self.bot.stop()
-            else:
-                self.bot.stop()
 
 if __name__ == '__main__' :
     try:
         drv = Driven()
         drv.load()
     except KeyboardInterrupt:
-        os.exit(drv)
+        sys.exit(drv)
